84dcf19a13c0651d8400cc83476a49c0b46110fb
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2023-05-29 12:50:59 +0100

    flight managament CRUD doneg

--- backend/src/main/java/com/dev/app/SpringBoot3TutorialApplication.java
+++ backend/src/main/java/com/dev/app/SpringBoot3TutorialApplication.java
@@ -2,8 +2,10 @@ package com.dev.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.dev.app.user", "com.dev.app.flight"})
 public class SpringBoot3TutorialApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/com/dev/app/flight/FlightController.java
@@ -0,0 +1,79 @@
+package com.dev.app.flight;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/flight")
+public class FlightController {
+    
+    @Autowired
+    private FlightRepository flightRepository;
+
+    public FlightController() {
+	}
+
+    @PostMapping
+    public ResponseEntity<Flight> createFlight(@RequestBody Flight flight) {
+        return new ResponseEntity<Flight>(flightRepository.save(flight), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Flight> getFlightById(@PathVariable Long id) {
+        Optional<Flight> flight = flightRepository.findById(id);
+        if (flight.isPresent()) {
+            return ResponseEntity.ok(flight.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }    
+    }
+
+    @GetMapping("/all")
+    public Iterable<Flight> getAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Flight> updateFlightById(@PathVariable Long id, @RequestBody Flight updatedFlight) {
+        Optional<Flight> flightOptional = flightRepository.findById(id);
+        if (flightOptional.isPresent()) {
+            Flight flight = flightOptional.get();
+            // Update the flight attributes with the values from the updatedFlight object
+            flight.setFlightNumber(updatedFlight.getFlightNumber());
+            flight.setAirline(updatedFlight.getAirline());
+            flight.setDepartureAirport(updatedFlight.getDepartureAirport());
+            flight.setArrivalAirport(updatedFlight.getArrivalAirport());
+            flight.setDepartureTime(updatedFlight.getDepartureTime());
+            flight.setArrivalTime(updatedFlight.getArrivalTime());
+            flight.setSeatsAvailable(updatedFlight.getSeatsAvailable());
+            flight.setPrice(updatedFlight.getPrice());
+    
+            return ResponseEntity.ok(flightRepository.save(flight));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Flight> deleteFlightById(@PathVariable Long id) {
+        Optional<Flight> flightOptional = flightRepository.findById(id);
+        if (flightOptional.isPresent()) {
+            flightRepository.deleteById(id);
+            return ResponseEntity.ok(flightOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/airline/{airline}")
+    public Iterable<Flight> getFlightsByAirline(@PathVariable String airline) {
+        return flightRepository.findByAirline(airline);
+    }
+
+
+}
