cdfb637a86aca4bfa1c824954c83bbea23366c54
BlueGob <moslemgannoun@gmail.com>
2023-05-30 23:04:39 +0100

    fixed cors error
     to disable cors add @CrossOrigin() on top of the controller

--- backend/src/main/java/com/dev/app/config/SecurityConfiguration.java
+++ backend/src/main/java/com/dev/app/config/SecurityConfiguration.java
@@ -22,6 +22,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
