538e5f7192feb91d6d5e61560116a0c297330fc4
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2023-05-30 05:06:01 +0200

    I improved validation + message from the server

--- backend/src/main/java/com/dev/app/SpringBoot3TutorialApplication.java
+++ backend/src/main/java/com/dev/app/SpringBoot3TutorialApplication.java
@@ -3,9 +3,11 @@ package com.dev.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan({"com.dev.app.user", "com.dev.app.flight", "com.dev.app.catering_companies"})
+// @ComponentScan("com.dev.app.exception")
 public class SpringBoot3TutorialApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/dev/app/config/SecurityConfiguration.java
+++ backend/src/main/java/com/dev/app/config/SecurityConfiguration.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -25,15 +27,28 @@ public class SecurityConfiguration {
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers( "/api/user/all").hasRole("ADMIN") // Secure the endpoint with role-based authorization
-                .requestMatchers( "/api/user/{id}").hasRole("ADMIN") // Secure the endpoint with role-based authorization
+                .requestMatchers("/api/user/all").hasRole("ADMIN") // Secure the endpoint with role-based authorization
+                .requestMatchers("/api/user/{id}").hasRole("ADMIN") // Secure the endpoint with role-based authorization
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler());
+
         return http.build();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType("application/json");
+            response.getWriter().write("{\"error\": \"ACCESS_DENIED:" + accessDeniedException.getMessage() + "\"}");
+            response.getWriter().flush();
+        };
     }
 }
